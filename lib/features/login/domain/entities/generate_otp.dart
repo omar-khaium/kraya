25e800f8a36f4153c90d79a1780548aa1f92df81
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 05:17:59 +0600

    constructor type refactored

--- lib/features/login/domain/entities/generate_otp.dart
+++ lib/features/login/domain/entities/generate_otp.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 class GenerateOtp extends Equatable {
   final String phone;
 
-  const GenerateOtp(this.phone);
+  const GenerateOtp({required this.phone});
 
   @override
   List<Object?> get props => [phone];
