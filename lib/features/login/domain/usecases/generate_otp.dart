686a4d3cbf13ac08b979249255e001bd2515bf34
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 05:26:05 +0600

    execute method added

--- lib/features/login/domain/usecases/generate_otp.dart
+++ lib/features/login/domain/usecases/generate_otp.dart
@@ -1,7 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:kraya/core/error/failures.dart';
+import 'package:kraya/features/login/domain/entities/generate_otp.dart';
 import 'package:kraya/features/login/domain/repositories/login.dart';
 
 class GenerateOtpUseCase {
   final LoginRepository repository;
 
   GenerateOtpUseCase(this.repository);
+
+  Future<Either<Failure, GenerateOtpEntity>> execute({required String phone}) async {
+    return await repository.generateOtp(phone);
+  }
 }
