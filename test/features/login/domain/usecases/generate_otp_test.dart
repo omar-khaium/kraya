aa3acdba37ddf274a73007514b4bd9f5159a7609
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 05:26:20 +0600

    generate otp test added

--- test/features/login/domain/usecases/generate_otp_test.dart
+++ test/features/login/domain/usecases/generate_otp_test.dart
@@ -1,1 +1,34 @@
+import 'dart:math';
 
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kraya/features/login/domain/entities/generate_otp.dart';
+import 'package:kraya/features/login/domain/repositories/login.dart';
+import 'package:kraya/features/login/domain/usecases/generate_otp.dart';
+import 'package:mockito/mockito.dart';
+
+class MockLoginRepository extends Mock implements LoginRepository {}
+
+void main() {
+  late GenerateOtpUseCase usecase;
+  late MockLoginRepository repository;
+
+  setUp(() {
+    repository = MockLoginRepository();
+    usecase = GenerateOtpUseCase(repository);
+  });
+
+  final phone = "123";
+  final generateOtp = GenerateOtpEntity(phone: "123");
+
+  test("should get response from login repository", () {
+    when(repository.generateOtp(phone)).thenAnswer((_) async => Right(generateOtp));
+
+    final result = usecase.execute(phone: phone);
+
+    expect(result, Right(generateOtp));
+
+    verify(repository.generateOtp(phone));
+    verifyNoMoreInteractions(repository);
+  });
+}
