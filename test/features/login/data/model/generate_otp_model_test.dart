fdf1a0b592ef3ccab7f368539d8a118e3bb7f825
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 19:23:54 +0600

    fromJson function added

--- test/features/login/data/model/generate_otp_model_test.dart
+++ test/features/login/data/model/generate_otp_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kraya/features/login/data/model/generate_otp_model.dart';
 import 'package:kraya/features/login/domain/entities/generate_otp.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   const generateOtpModel = GenerateOtpModel(phone: "123");
@@ -8,5 +12,16 @@ void main() {
   test("should be a subclass of GenerateOtpEntity entity", () {
     // assert
     expect(generateOtpModel, isA<GenerateOtpEntity>());
+  });
+
+  test("should be able to parse generate_otp.json with GenerateOtpModel.fromJson()", () {
+    // arrange
+    final Map<String, dynamic> jsonMap = json.decode(FixtureReader.instance.read("generate_otp.json"));
+
+    // act
+    final result = GenerateOtpModel.fromJson(jsonMap);
+
+    // assert
+    expect(result, generateOtpModel);
   });
 }
