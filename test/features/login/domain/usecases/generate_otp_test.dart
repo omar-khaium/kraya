588bcff13ff4b59a030202880acf361e9c2e633e
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:11:22 +0600

    test refactored

--- test/features/login/domain/usecases/generate_otp_test.dart
+++ test/features/login/domain/usecases/generate_otp_test.dart
@@ -1,34 +1,36 @@
-import 'dart:math';
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kraya/features/login/domain/entities/generate_otp.dart';
 import 'package:kraya/features/login/domain/repositories/login.dart';
 import 'package:kraya/features/login/domain/usecases/generate_otp.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-class MockLoginRepository extends Mock implements LoginRepository {}
+import 'generate_otp_test.mocks.dart';
 
+class LoginRepositoryTest extends Mock implements LoginRepository {}
+
+@GenerateMocks([LoginRepositoryTest])
 void main() {
   late GenerateOtpUseCase usecase;
-  late MockLoginRepository repository;
+  late MockLoginRepositoryTest mockRepository;
 
   setUp(() {
-    repository = MockLoginRepository();
-    usecase = GenerateOtpUseCase(repository);
+    mockRepository = MockLoginRepositoryTest();
+    usecase = GenerateOtpUseCase(mockRepository);
   });
 
   const phone = "123";
   const entity = GenerateOtpEntity(phone: "123");
 
   test("should get response from login repository", () async {
-    when(repository.generateOtp(phone)).thenAnswer((_) async => const Right(entity));
+    when(mockRepository.generateOtp(phone)).thenAnswer((_) async => const Right(GenerateOtpEntity(phone: phone)));
 
     final result = await usecase.execute(phone: phone);
 
     expect(result, const Right(entity));
 
-    verify(repository.generateOtp(phone));
-    verifyNoMoreInteractions(repository);
+    verify(mockRepository.generateOtp(phone));
+    verifyNoMoreInteractions(mockRepository);
   });
 }
