095f54704978ef1cc7492e333beb7cb08b27f365
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:10:23 +0600

    test skeleton refactored

--- test/features/login/domain/entities/generate_otp.dart
+++ test/features/login/domain/entities/generate_otp.dart
@@ -1,7 +1,1 @@
-import 'package:equatable/equatable.dart';
-
-class GenerateOtp extends Equatable {
-  @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
-}
+void main() {}
