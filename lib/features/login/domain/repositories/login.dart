a78abc0a9cd47d2f47545e289155379a529a0a8e
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:09:32 +0600

    line formatted

--- lib/features/login/domain/repositories/login.dart
+++ lib/features/login/domain/repositories/login.dart
@@ -5,5 +5,6 @@ import '../entities/generate_otp.dart';
 
 abstract class LoginRepository {
   Future<Either<Failure, GenerateOtpEntity>> generateOtp(final String phone);
+
   Future<Either<Failure, GenerateOtpEntity>> resendOtp(final String phone);
 }
