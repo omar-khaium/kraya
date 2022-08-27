573efa89ed394ff43452210a8fc68a2c4de2457a
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:30:55 +0600

    usecase implementation refactored

--- lib/features/login/domain/usecases/generate_otp.dart
+++ lib/features/login/domain/usecases/generate_otp.dart
@@ -1,15 +1,23 @@
 import 'package:dartz/dartz.dart';
+import 'package:kraya/core/usecase/usecase.dart';
 
 import '../../../../core/error/failures.dart';
 import '../entities/generate_otp.dart';
 import '../repositories/login.dart';
 
-class GenerateOtpUseCase {
+class GenerateOtpUseCase implements Usecase<GenerateOtpEntity, Params> {
   final LoginRepository repository;
 
   GenerateOtpUseCase(this.repository);
 
-  Future<Either<Failure, GenerateOtpEntity>> call({required String phone}) async {
-    return await repository.generateOtp(phone);
+  @override
+  Future<Either<Failure, GenerateOtpEntity>> call(Params params) async {
+    return await repository.generateOtp(params.phone);
   }
+}
+
+class Params {
+  final String phone;
+
+  Params({required this.phone});
 }
