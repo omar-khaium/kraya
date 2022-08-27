946dd494edff1ce5f4392a53bf63cc52fc2acedd
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:49:43 +0600

    imports refactored

--- lib/features/login/domain/usecases/verify_otp.dart
+++ lib/features/login/domain/usecases/verify_otp.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:kraya/core/usecase/usecase.dart';
 
 import '../../../../core/error/failures.dart';
+import '../../../../core/usecase/usecase.dart';
 import '../entities/verify_otp.dart';
 import '../repositories/login.dart';
 
