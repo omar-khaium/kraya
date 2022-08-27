c3d7e00377c99023093e855f128d116178e5fe41
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 19:16:59 +0600

    generate otp model added to production code

---
+++ lib/features/login/data/model/generate_otp_model.dart
@@ -0,0 +1,5 @@
+import 'package:kraya/features/login/domain/entities/generate_otp.dart';
+
+class GenerateOtpModel extends GenerateOtpEntity {
+  const GenerateOtpModel({required String phone}) : super(phone: phone);
+}
