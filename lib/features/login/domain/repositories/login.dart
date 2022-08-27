911817973df4c8be213f51e273693619e75b9dbc
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:40:39 +0600

    verify otp method added to repository

--- lib/features/login/domain/repositories/login.dart
+++ lib/features/login/domain/repositories/login.dart
@@ -2,9 +2,12 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/failures.dart';
 import '../entities/generate_otp.dart';
+import '../entities/verify_otp.dart';
 
 abstract class LoginRepository {
   Future<Either<Failure, GenerateOtpEntity>> generateOtp(final String phone);
 
   Future<Either<Failure, GenerateOtpEntity>> resendOtp(final String phone);
+
+  Future<Either<Failure, VerifyOtpEntity>> verifyOtp(final String phone, final String code);
 }
