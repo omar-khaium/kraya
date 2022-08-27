0176a86c07a8c77642e288b0d4215c4440768626
Omar khaium Chowhdury <omarkhaium.chowdhury@gmail.com>
2022-08-27 14:10:08 +0600

    test skeleton refactored

--- test/features/login/domain/repositories/login.dart
+++ test/features/login/domain/repositories/login.dart
@@ -1,9 +1,1 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/error/failures.dart';
-import '../entities/generate_otp.dart';
-
-abstract class LoginRepository {
-  Future<Either<Failure, GenerateOtp>> generateOtp(final String number);
-  Future<Either<Failure, GenerateOtp>> resendOtp(final String number);
-}
+void main() {}
