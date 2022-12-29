53f7f26d92229693bb6a3deb60a98a6b390b548d
tomal9924 <tomalahmed350@gmail.com>
2022-12-29 19:24:27 +0600

    add bills UI done

--- lib/core/widget_custom_menu_card.dart
+++ lib/core/widget_custom_menu_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kraya/core/colors.dart';
 import 'package:kraya/core/enums.dart';
@@ -5,10 +6,12 @@ import 'package:kraya/core/text_style.dart';
 
 class WidgetMenuCard extends StatelessWidget {
   final String text;
+  final int amount;
   final IconData iconData;
   final Function onTap;
 
-  WidgetMenuCard({Key? key, required this.text, required this.iconData, required this.onTap}) : super(key: key);
+  WidgetMenuCard({Key? key, required this.text, required this.iconData, required this.onTap, required this.amount})
+      : super(key: key);
 
   PropertySelection propertyType = PropertySelection.none;
 
@@ -18,29 +21,61 @@ class WidgetMenuCard extends StatelessWidget {
       onTap: () {
         onTap();
       },
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: 88,
-        child: PhysicalModel(
-          color: ColorSystem.instance.cardDeep,
-          elevation: 2,
-          borderRadius: BorderRadius.circular(8),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                iconData,
-                color: ColorSystem.instance.primary,
+      child: PhysicalModel(
+        color: ColorSystem.instance.cardDeep,
+        elevation: 7,
+        borderRadius: BorderRadius.circular(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    height: 36,
+                    width: 36,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(48),
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [ColorSystem.instance.gradientStart, ColorSystem.instance.gradientEnd],
+                      ),
+                    ),
+                    child: Icon(
+                      iconData,
+                      color: ColorSystem.instance.background,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    text,
+                    style: TextSystem.instance.verySmall(ColorSystem.instance.text),
+                  ),
+                ],
               ),
-              const SizedBox(height: 4),
-              Text(
-                text,
-                style: TextSystem.instance.small(ColorSystem.instance.primary),
-              )
-            ],
-          ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(child: CupertinoSwitch(value: true, onChanged: (value) {},activeColor: ColorSystem.instance.primary,)),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      "৳ ${amount.toString()}",
+                      style: TextSystem.instance.large(ColorSystem.instance.primary),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/ui/screen/screen_utility_bills.dart
+++ lib/ui/screen/screen_utility_bills.dart
@@ -8,13 +8,16 @@ import 'package:kraya/core/custom_app_bar.dart';
 import 'package:kraya/core/enums.dart';
 import 'package:kraya/core/gradient_button.dart';
 import 'package:kraya/core/text_style.dart';
+import 'package:kraya/core/widget_custom_menu_card.dart';
 import 'package:kraya/model/bill.dart';
 import 'package:kraya/ui/widgets/alert_add_bills.dart';
+import 'package:kraya/ui/widgets/login/create_account/widget_input_text.dart';
 import 'package:kraya/ui/widgets/login/verify_otp/widget_back_button.dart';
 import 'package:kraya/ui/widgets/login/widget_language_toggle.dart';
 import 'package:kraya/ui/widgets/login/widget_login_form.dart';
 import 'package:kraya/ui/widgets/login/widget_login_gradient.dart';
 import 'package:kraya/ui/widgets/login/widget_login_greetings.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class UtilityBillsScreen extends StatefulWidget {
   const UtilityBillsScreen({super.key});
@@ -24,12 +27,17 @@ class UtilityBillsScreen extends StatefulWidget {
 }
 
 class _UtilityBillsScreenState extends State<UtilityBillsScreen> {
+  final TextEditingController billNameController = TextEditingController();
+  final TextEditingController billAmountController = TextEditingController();
   final List<Bill> bills = [];
   String billName = '';
   int billAmount = 0;
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 5;
+    final double itemWidth = size.width / 1.8;
     return Scaffold(
       backgroundColor: ColorSystem.instance.background,
       body: Column(
@@ -39,12 +47,24 @@ class _UtilityBillsScreenState extends State<UtilityBillsScreen> {
           Expanded(
             child: GridView.builder(
               itemCount: bills.length,
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 24),
               itemBuilder: (_, index) {
-                return Container(
-                  child: Text(billName),
+                final Bill bill = bills[index];
+                return WidgetMenuCard(
+                  text: bill.billName,
+                  iconData: Icons.electric_bolt,
+                  onTap: () {},
+                  amount: bill.amount,
                 );
               },
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: (itemWidth / itemHeight),
+              ),
             ),
           )
         ],
@@ -63,12 +83,40 @@ class _UtilityBillsScreenState extends State<UtilityBillsScreen> {
               ),
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
-            builder: (_) => AlertAddBills(
-              onTap: (billNAME, amount) {
-                billName = billNAME;
-                billAmount = amount;
-                bills.add(Bill.name(billName, billAmount));
-              },
+            builder: (_) => Padding(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      child: Text(
+                        "Add Bill",
+                        style: TextSystem.instance.large(ColorSystem.instance.text),
+                      ),
+                    ),
+                    WidgetInput(
+                        label: "Bill name", controller: billNameController, icon: Icons.badge_outlined, type: TextInputType.text),
+                    WidgetInput(
+                        label: "Amount", controller: billAmountController, icon: MdiIcons.currencyBdt, type: TextInputType.text),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    GradientButton(
+                        onPressed: () {
+                          setState(() {
+                            bills.add(Bill.name(billNameController.text, int.parse(billAmountController.text)));
+                            billNameController.text="";
+                            billAmountController.text="";
+                            Navigator.of(context).pop();
+                          });
+                        },
+                        text: "Submit")
+                  ],
+                ),
+              ),
             ),
           );
         },
