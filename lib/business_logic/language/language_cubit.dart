babb3d7441509f8f44e0c3ff34ab6dba26f72764
Tanvir Ahmed <tomalahmed350@gmail.com>
2023-06-13 17:49:37 +0600

    language feature done

---
+++ lib/business_logic/language/language_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'language_state.dart';
+
+class LanguageCubit extends Cubit<LanguageState> {
+  LanguageCubit() : super(LanguageInitial());
+}
